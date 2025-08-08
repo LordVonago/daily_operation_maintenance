148f590a63e64d0b29a6e7e89ae06e7fcdf4ca89
LordVonago <LordVonago@users.noreply.github.com>
2025-08-08 18:54:05 -0300

    Initial commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import classes from "./Home.module.css";
+
+const Home = () => {
+  let categories = [
+    "Segurança",
+    "5S",
+    "Pessoas",
+    "Equipamentos",
+    "Manutenção",
+    "Limpeza",
+    "Revisão",
+    "Outros",
+  ];
+
+  const [inputdatevalue, setinputdatevalue] = useState("");
+
+  useEffect(() => {
+    let date = new Date();
+    date = date.toISOString().split("T")[0];
+    setinputdatevalue(date);
+
+    if (sessionStorage.getItem("date")) {
+      return;
+    } else {
+      sessionStorage.setItem("date", date);
+    }
+  }, []);
+
+  return (
+    <>
+      <div className={classes.mainContainer}>
+        {/* Título */}
+        <div className={classes.NavBar}>
+          <div className={classes.logoContainer}>
+            <img src="/images.png" alt="" />
+          </div>
+          <section className={classes.title}>
+            <h1>Diário de bordo</h1>
+          </section>
+        </div>
+
+        {/* Conteúdo */}
+        <div className={classes.pageContainer}>
+          {/* Barra de navegação (Filtros de data) */}
+          <aside className={classes.leftAside}>
+            <section>
+              <h3>Data:</h3>
+              <input
+                type="date"
+                name="date"
+                id="date"
+                value={sessionStorage.getItem("date") ?? inputdatevalue}
+                onChange={(e) => {
+                  setinputdatevalue(e.target.value);
+                  sessionStorage.setItem("date", e.target.value);
+                }}
+              />
+            </section>
+
+            {/* Barra de navegação (categorias) */}
+            <section className={classes.categories}>
+              <h3>Categorias:</h3>
+              {categories.map((element, index) => (
+                <p key={index}>{element}</p>
+              ))}
+            </section>
+          </aside>
+
+          {/* Conteúdo das categorias ((((WIP)))) */}
+          <div className={classes.content}>
+            <p>Content</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
